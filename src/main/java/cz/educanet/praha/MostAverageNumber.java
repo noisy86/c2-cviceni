04bcca4fe2109e2c634bdbb8df21a75e8c612bf8
noisy86 <platon12345546@gmail.com>
2021-04-26 14:25:14 +0200

    mam

--- src/main/java/cz/educanet/praha/MostAverageNumber.java
+++ src/main/java/cz/educanet/praha/MostAverageNumber.java
@@ -1,10 +1,33 @@
 package cz.educanet.praha;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MostAverageNumber {
 
     static int findMostAverageNumber(Scanner sc) {
-        return 0;
+        double total = 0;
+        int count = 0;
+        int average = 0;
+        int closesest = 0;
+        List<Integer> nums = new ArrayList<>();
+        while (sc.hasNext()){
+            int cislo = sc.nextInt();
+            total = total + cislo;
+            nums.add(cislo);
+            count++;
+            if(count > 0){
+                average = (int) total/count;
+            }
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (Math.abs(nums.get(i) - average) <= Math.abs(closesest - average)) {
+                closesest = nums.get(i);
+
+            }
+        }
+
+        return closesest;
     }
 }
